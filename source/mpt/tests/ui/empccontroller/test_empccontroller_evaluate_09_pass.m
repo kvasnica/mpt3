8c82b9f6d261bd70dc7386da8b83e40b96beed21
Michal Kvasnica <michal.kvasnica@stuba.sk>
2013-05-19 01:16:23 +0200

    test_empccontroller_evaluate_0[8,9]_pass: also stress-test outside points

--- source/mpt/tests/ui/empccontroller/test_empccontroller_evaluate_09_pass.m
+++ source/mpt/tests/ui/empccontroller/test_empccontroller_evaluate_09_pass.m
@@ -7,9 +7,19 @@ E = EMPCController(model, probStruct.N);
 E.display();
 assert(E.nr==10);
 assert(numel(E.optimizer)==4);
+
+% only points inside
 X = PolyUnion(E.optimizer.convexHull).convexHull.grid(35);
 assert(size(X, 1)==1209);
+t=clock;
+for i = 1:size(X, 1),
+	u = E.evaluate(X(i, :)');
+end,
+etime(clock, t)
 
+% also points outside
+X = E.optimizer.convexHull.outerApprox().grid(40);
+assert(size(X, 1)==1600);
 t=clock;
 for i = 1:size(X, 1),
 	u = E.evaluate(X(i, :)');
