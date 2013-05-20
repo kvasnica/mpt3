c5a08aa8e0ecf50d26797deb368f681bcbbb05a9
Michal Kvasnica <michal.kvasnica@stuba.sk>
2013-05-20 13:54:19 +0200

    Polyhedron/facetInteriorPoints: reject arrays
    P.facetInteriorPoints() with "P" an array now fails. Use
      x = P.forEach(@(e) e.facetInteriorPoints, 'UniformOutput', false)
    instead.
    
    This change is part of an ongoing effort to eliminate manual per-element
    implementation in Polyhedron methods.

--- source/mpt/tests/poly/test_polyhedron_facetInteriorPoints_04_pass.m
+++ source/mpt/tests/poly/test_polyhedron_facetInteriorPoints_04_pass.m
@@ -7,7 +7,7 @@ P(1) = ExamplePoly.randHrep('d',6);
 P(2) = ExamplePoly.randVrep('d',4);
 
 % get all points
-s = P.facetInteriorPoints;
+s = P.forEach(@(e) e.facetInteriorPoints, 'UniformOutput', false);
 
 for i=1:2
     if any(~P(i).contains(s{i}'))
