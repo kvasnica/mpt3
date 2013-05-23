c74f228353b3210a0fa04eafbd74a85fc39887c8
Michal Kvasnica <michal.kvasnica@stuba.sk>
2013-05-23 12:12:20 +0200

    test_convexset_fplot_07_pass: trully test fplot()

--- source/mpt/tests/convexset/test_convexset_fplot_07_pass.m
+++ source/mpt/tests/convexset/test_convexset_fplot_07_pass.m
@@ -8,19 +8,22 @@ F1 = [-1<=x<=5] + [0.5*x^2<=0.2];
 Y1 = YSet(x,F1,sdpsettings('verbose',0));
 F2 = [x<=0.3] + [-x^2>=-.4];
 Y2 = YSet(x,F2,sdpsettings('verbose',0));
-
 Y = [Y1,Y2];
 
 A1 = AffFunction(rand(3,1),rand(3,1));
 A2 = AffFunction(rand(4,1),rand(4,1));
 Q = QuadFunction(rand(1,1,2),rand(2,1),rand(2,1));
 
+Y.addFunction(A1,'A1');
+Y.addFunction(A1,'A2');
+Y.addFunction(A1,'Q');
+
 % must get two handles
-h = Y.addFunction(A1,'A1');
+h = Y.fplot('A1');
 assert(numel(h)==2);
-h = Y.addFunction(A2,'A2');
+h = Y.fplot('A2');
 assert(numel(h)==2);
-h = Y.addFunction(Q,'Q');
+h = Y.fplot('Q');
 assert(numel(h)==2);
 
 close all
