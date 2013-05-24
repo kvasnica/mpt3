8fc81cee18c3a21f88595b5e96b6b0051575012d
Michal Kvasnica <michal.kvasnica@stuba.sk>
2013-05-24 22:47:35 +0200

    test_polyhedron_project_03_pass: test rejection of arrays

--- source/mpt/tests/poly/test_polyhedron_project_03_pass.m
+++ source/mpt/tests/poly/test_polyhedron_project_03_pass.m
@@ -5,7 +5,15 @@ function  test_polyhedron_project_03_pass
 
 P(1) = ExamplePoly.randVrep;
 P(2) = ExamplePoly.randHrep;
-d = P.project([12;30]);
+
+% arrays are rejected because size of output from Polyhedron/project
+% depends on cardinality of "y"
+[worked, msg] = run_in_caller('d = P.project([12;30]);');
+assert(~worked);
+asserterrmsg(msg, 'This method does not support arrays. Use the forEach() method.');
+
+% here we have a single point, so output from forEach is uniform
+d = P.forEach(@(elem) elem.project([12;30]));
 
 for i=1:2
     if isempty(d(i).dist)
