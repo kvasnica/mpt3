c1d6e737f343c92f9f4f03eba4c703ef3b081f14
Michal Kvasnica <michal.kvasnica@stuba.sk>
2013-05-26 00:17:44 +0200

    test_convexset_plot_09_pass: also test negative bounds

--- source/mpt/tests/convexset/test_convexset_plot_09_pass.m
+++ source/mpt/tests/convexset/test_convexset_plot_09_pass.m
@@ -1,6 +1,7 @@
 function test_convexset_plot_09_pass
-% plotting of 1D sets with positive lower bounds
+% plotting of 1D sets where both bounds are either positive or negative
 
+% both bounds positive
 lb = 1.1;
 ub = 10.8;
 x = sdpvar(1, 1); 
@@ -9,12 +10,21 @@ h = Y.plot();
 % check that a correct set was plotted
 assert(isequal(get(h, 'XData'), [lb ub]));
 
-
 x = sdpvar(1, 1);
 Y = YSet(x, [ (x-4)^2 <= 1.5 ]);
 h = Y.plot();
 bounds = get(h, 'XData');
 good = [2.77525512860841 5.22474487139159];
 assert(norm(bounds-good, Inf)<1e-10);
+
+% both bounds negative
+lb = -4;
+ub = -2.3;
+x = sdpvar(1, 1); 
+Y = YSet(x, [lb <= x <= ub]); 
+h = Y.plot();
+assert(isequal(get(h, 'XData'), [lb ub]));
+
+close 
 
 end
