6dbe9416c0baa88344043c5220153c40a84ba398
Michal Kvasnica <michal.kvasnica@stuba.sk>
2013-05-31 21:26:04 +0200

    SystemSignal/filter_reference: allow symbolic references
    signal.reference='free' now enables tracking of time-varying references.
    
    During construction of the MPC problem, such references are treated as
    symbolic variables whose actual variables are specified while calling
    controller.evaluate().
    
    related to issue #21

--- source/mpt/modules/ui/@SystemSignal/filter_reference.m
+++ source/mpt/modules/ui/@SystemSignal/filter_reference.m
@@ -3,10 +3,61 @@ function filter = filter_reference(obj)
 
 % set up the filter
 filter = FilterSetup;
-filter.addField('value', zeros(obj.n, 1), @isnumeric);
+filter.addField('value', zeros(obj.n, 1));
 
 % the filter impacts the following calls:
 filter.callback('set') = @on_set;
+filter.callback('getInitVariables') = @on_getInitVariables;
+filter.callback('getVariables') = @on_variables;
+filter.callback('instantiate') = @on_instantiate;
+filter.callback('uninstantiate') = @on_uninstantiate;
+
+end
+
+%------------------------------------------------
+function out = on_getInitVariables(obj, varargin)
+% called when variables used to initialize the optimization problem are
+% requested
+
+if obj.internal_properties.reference.free
+	out.name = obj.name;
+	out.filter = 'reference';
+	out.var = obj.internal_properties.reference.var;
+else
+	out = [];
+end
+
+end
+
+%------------------------------------------------
+function out = on_variables(obj, varargin)
+% called when filter's variables are requested
+
+if obj.internal_properties.reference.free
+	out = obj.internal_properties.reference.var;
+else
+	out = [];
+end
+
+end
+
+%------------------------------------------------
+function out = on_instantiate(obj, varargin)
+% called after the object was instantiated
+
+if obj.internal_properties.reference.free
+	obj.internal_properties.reference.var = sdpvar(obj.n, 1);
+end
+out = [];
+
+end
+
+%------------------------------------------------
+function out = on_uninstantiate(obj, varargin)
+% called when the YALMIP representation of variables is removed
+
+obj.internal_properties.reference.var = [];
+out = [];
 
 end
 
@@ -14,9 +65,24 @@ end
 function obj = on_set(obj, value)
 % called when the reference is to be changed
 
-if size(value, 1) ~= obj.n
-	error('The refence must have %d rows.', obj.n);
+
+if isa(value, 'double')
+	if ~isempty(value) && (size(value, 1) ~= obj.n)
+		error('The refence must have %d rows.', obj.n);
+	end
+	obj.internal_properties.reference.free = false;
+	
+elseif isa(value, 'char')
+	value = lower(value);
+	if ~isequal(value, 'free')
+		error('The value of reference must be ''free''.');
+	end
+	obj.internal_properties.reference.free = true;
+	
+else
+	error('Value of "reference" must be either a double or a string.');
 end
+
 obj.reference = value;
 
 end
