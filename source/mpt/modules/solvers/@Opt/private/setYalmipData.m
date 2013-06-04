b165fa2fa5198fc9c8d04e901477da209a10a481
Michal Kvasnica <michal.kvasnica@stuba.sk>
2013-06-04 20:21:16 +0200

    Opt/setYalmipData: use solvesdp() to get what we need
    Instead of manually replicating yalmip's core functionality (which
    was buggy anyhow with respect to ordering of parametric variables
    before June 4, 2013), we can get interface data with all fields set
    as expected by simply calling solvesdp() with options.pureexport=true.

--- source/mpt/modules/solvers/@Opt/private/setYalmipData.m
+++ source/mpt/modules/solvers/@Opt/private/setYalmipData.m
@@ -17,24 +17,10 @@ end
 
 if nargin < 5, error('Opt: Syntax : opt.setYalmipData(con, obj, th, u)'); end
 
-
-% We use quadprog here in order to get a consistent model structure
-[interfacedata,recoverdata,solver,diagnostic,F,Fremoved] = compileinterfacedata(con,[],[],obj,sdpsettings,0,true);
-if isempty(interfacedata)
-    error('Opt: Could not create yalmip model')
-end
-
-% Get the parametric variables
-temp = [];
-for i = 1:length(th), temp = [temp;getvariables(th(i))]; end
-% respect ordering of parametric variables!
-[~, interfacedata.parametric_variables] = ismember(temp, recoverdata.used_variables);
-
-% Get the optimization variables
-interfacedata.requested_variables = [];
-for i = 1:length(u)
-    interfacedata.requested_variables = [interfacedata.requested_variables;find(ismember(recoverdata.used_variables,getvariables(u(i))))];
-end
+% call solvesdp with options.pureexport=true to get the parsed model
+options = sdpsettings;
+options.pureexport = true;
+interfacedata = solvesdp(con, obj, options, th, u);
 
 % Convert from Yalmip => mpt2.6 => mpt3 format
 mat = yalmip2mpt(interfacedata);
@@ -43,12 +29,5 @@ mat.lb(mat.lb==-Inf) = -MPTOPTIONS.infbound;
 mat.ub(mat.ub==Inf) = MPTOPTIONS.infbound;
 mat.ub(mat.ub==-Inf) = -MPTOPTIONS.infbound;
 opt = opt.setMPT26Data(mat);
-
-%       [model,rmodel,dd,internal] = export(con,obj,sdpsettings('solver','mpt'));
-%       vars = find(ismember(internal.used_variables,getvariables(x)));
-%       internal.parametric_variables = vars;
-%       vars = find(ismember(internal.used_variables,getvariables(u)));
-%       internal.requested_variables = vars;
-
 
 end
