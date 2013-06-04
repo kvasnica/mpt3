1de52c56d2f6955416060444c41160fe2679b288
Michal Kvasnica <michal.kvasnica@stuba.sk>
2013-06-04 16:10:27 +0200

    test_empccontroller_tracking_03: check correctness of coordinate system
    yalmip versions prior to June 4, 2013 did not order parametric
    variables correctly, which lead to parametric solutions with wrong
    coordinate systems
    
    related to 64c2aadb91d6

--- source/mpt/tests/ui/empccontroller/test_empccontroller_tracking_03_pass.m
+++ source/mpt/tests/ui/empccontroller/test_empccontroller_tracking_03_pass.m
@@ -35,6 +35,13 @@ assert(abs(u_on - ugood) <= 1e-8);
 explicit = online.toExplicit();
 assert(explicit.nr==13); % 31 without bounding the reference
 
+% make sure the ordering of parametric variables is correct (used to be a
+% bug in Opt/private/setYalmipData and in YALMIP itself)
+x0 = explicit.partition.Set(1).chebyCenter.x;
+% the third coordinate should be the input, which is bounded by +/- 1 (see
+% explicit.xinitFormat for ordering);
+assert((x0(3)<=1) && (x0(3)>=-1));
+
 % bogus settings must alert the user
 [~, msg] = run_in_caller('explicit.evaluate(x0)');
 asserterrmsg(msg, 'Please provide initial value of "u.reference".');
