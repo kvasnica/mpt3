899ccb9122d701c80068daf778706f014a07fe50
Michal Kvasnica <michal.kvasnica@stuba.sk>
2013-07-10 14:06:01 +0200

    removing EMPCController/toSearchTree()
    the method is superseded by EMPCController/binaryTree()

--- source/mpt/modules/ui/@EMPCController/toSearchTree.m
+++
@@ -1,9 +0,0 @@
-function ctrlst = toSearchTree(obj)
-%
-% construct an explicit controller with binary search tree for fast
-% evaluation of the control law
-%
-
-ctrlst = EMPCSTController(obj);
-
-end
