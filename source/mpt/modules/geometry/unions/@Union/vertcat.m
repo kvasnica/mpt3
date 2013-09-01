28085610af10d887e20a7d26581fe9356d303ea3
Michal Kvasnica <michal.kvasnica@stuba.sk>
2013-09-01 21:37:47 +0200

    Union/vertcat: significantly improve performance
    Same as 85bd4ab88fb9 and issue #87 but for Unions. Improves performance
    by a factor of 2 to 5.

--- source/mpt/modules/geometry/unions/@Union/vertcat.m
+++ source/mpt/modules/geometry/unions/@Union/vertcat.m
@@ -1,24 +1,35 @@
 function y=vertcat(varargin)
-%
-% avoids concatenation into matrices
-%
+% Vertical concatenation of Union objects
 
-% check for empty arguments
-e = cellfun(@(x)builtin('isempty',x),varargin,'UniformOutput',false);
+% Since concatenation is a time-critical operation which must be
+% implemented as efficiently as possible:
+% * use cellfun('isempty') instead of cellfun(@isempty)
+% * don't use cellfun(..., 'UniformOutput', false), it's slow due to
+% parsing of the options!
+% * don't use anonymous function handles in cellfun(), it's slow!
+% * don't use "parfor", it's slow!
+% * don't use unique(cell), it's slow!
 
 % delete empty entries
-varargin(cell2mat(e))=[];
+e = cellfun('isempty', varargin);
+varargin(e)=[];
 
-% check if the sets are the same
-s = cellfun(@class,varargin,'UniformOutput',false);
-if length(unique(s))~=1
-   error('Only the objects of the same type can be concatenated.');
+% check whether all arguments are of the same type
+first_class = class(varargin{1});
+for i = 2:numel(varargin)
+	if ~isequal(class(varargin{i}), first_class)
+		error('Only the objects of the same type can be concatenated.');
+	end
 end
 
-parfor i=1:length(varargin)
-    if size(varargin{i},2)>size(varargin{i},1)
-        varargin{i} = transpose(varargin{i});
-    end
+% make sure that each argument is a column array
+for i = 1:length(varargin)
+	if size(varargin{i}, 2) > 1
+		varargin{i} = varargin{i}(:);
+	end
 end
 
+% concatenate arguments vertically
 y = builtin('vertcat',varargin{:});
+
+end
