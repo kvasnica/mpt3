3bd1060d4d434c4c950fe819f358de1c2b2434db
Michal Kvasnica <michal.kvasnica@stuba.sk>
2014-07-07 14:18:47 +0200

    Polyhedron/itersect: remove dead code

--- source/mpt/modules/geometry/sets/@Polyhedron/intersect.m
+++ source/mpt/modules/geometry/sets/@Polyhedron/intersect.m
@@ -73,32 +73,4 @@ else
 	PnS = Polyhedron('H', [P.H_int;S.H_int], 'He', [P.He_int;S.He_int]);
 end
 
-
-% if P.hasHRep
-%   % Cheaper to just compute the convex hull for low-dimensions
-% %   if ~S.hasHRep && S.Dim < 4, S.minHRep(); end 
-%   
-%   if S.hasHRep
-%     PnS = Polyhedron('H', [P.H;S.H], 'He', [P.He;S.He]);
-%   else
-%     % Hrep P cap Vrep S
-%     % Form Polyhedron Q = {[A 0; 0 -I]*[x;lam;gam] <= [b;0], 
-%     % [Ae 0 0;-I V' R';0 1' 0]*[x;lam;gam] = [e;0;1]}
-%     nV = size(S.V,1); nR = size(S.R,1);
-%     H  = [P.A zeros(size(P.A,1),nV+nR) P.b];
-%     H  = [H;zeros(nV+nR,P.Dim) -eye(nV+nR) zeros(nV+nR,1)];
-%     He = [P.Ae zeros(size(P.Ae,1),nV+nR) P.be;...
-%       -eye(P.Dim) S.V' S.R' zeros(P.Dim,1);...
-%       zeros(1,P.Dim) ones(1,nV) zeros(1,nR) 1];
-%     Q = Polyhedron('H', H, 'He', He);
-%     PnS = Q.projection(1:P.Dim);
-%   end
-% else
-%   if S.hasHRep
-%     PnS = intersect(S,P);
-%   else % Vrep P cap Vrep S
-%     error('Intersection of two Vrep polyhedra not implemented yet');
-%   end
-% end
-
 end
