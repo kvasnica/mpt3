75ebf3ff3ca8ef04f227fc3488f7f5cf0176c769
Michal Kvasnica <michal.kvasnica@stuba.sk>
2016-11-06 18:58:17 +0100

    test_polyhedron_fplot_04: do not pass negative values to sqrt
    Due to numerical tolerances in vertex enumeration with lb=[0;0] it could
    happen that very small negative numbers are generated (e.g. -1e-15),
    making sqrt() inoperable.

--- source/mpt/tests/poly/test_polyhedron_fplot_04_pass.m
+++ source/mpt/tests/poly/test_polyhedron_fplot_04_pass.m
@@ -4,7 +4,7 @@ function test_polyhedron_fplot_04_pass
 %
 
 P = Polyhedron('lb',[0;0],'ub',[5;5],'He',[0.1 -5.2 0 ]);
-F = Function(@(x)sqrt(x));
+F = Function(@(x)sqrt(abs(x))); % prevent failing on vertices like -1e-15
 P.addFunction(F,'sqr');
 
 P.fplot();
