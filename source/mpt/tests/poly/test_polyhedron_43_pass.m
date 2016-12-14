be8359aede493f273875c507943489a69a2f0c98
Michal Kvasnica <michal.kvasnica@stuba.sk>
2016-12-14 10:15:08 +0100

    test_polyhedron_43_pass: allow rays to have oposite sign
    The space spanned by the rays is the same if the rays are negated.

--- source/mpt/tests/poly/test_polyhedron_43_pass.m
+++ source/mpt/tests/poly/test_polyhedron_43_pass.m
@@ -3,15 +3,17 @@ function test_polyhedron_43_pass
 
 % unbounded half-space
 P = Polyhedron([1 0], 0);
+P.minVRep();
 Rexpected = [1 0; 0 1; 0 -1];
 %assert(isempty(P.V)); % no vertices
-assert(isequal(sortrows(P.R), sortrows(Rexpected)));
+assert(isequal(sortrows(P.R), sortrows(Rexpected)) || isequal(sortrows(P.R), sortrows(-Rexpected)));
 
 % unbounded half-space
 P = Polyhedron([1 0], 1);
+P.minVRep();
 Rexpected = [1 0; 0 1; 0 -1];
 Vexpected = [1 0];
 assert(isequal(P.V, Vexpected));
-assert(isequal(sortrows(P.R), sortrows(Rexpected)));
+assert(isequal(sortrows(P.R), sortrows(Rexpected)) || isequal(sortrows(P.R), sortrows(-Rexpected)));
 
 end
