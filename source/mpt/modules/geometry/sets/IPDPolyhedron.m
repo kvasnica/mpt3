12002a5caa3e0eb7f30f0d2923cdc535a77dcdf2
Michal Kvasnica <michal.kvasnica@stuba.sk>
2017-10-05 07:28:06 +0200

    IPDPolyhedron/isEmptySet: determine emptiness via feasibility LP
    It's faster than converting to Polyhedron first and then using
    isEmptySet() on top of that.
    
    This change also introduces IPDPolyhedron/getHalfspaces() as a protected
    method that returns the half-space representation of the set.

--- source/mpt/modules/geometry/sets/IPDPolyhedron.m
+++ source/mpt/modules/geometry/sets/IPDPolyhedron.m
@@ -19,21 +19,11 @@ classdef IPDPolyhedron < Polyhedron
                     P(i) = obj(i).Internal.Polyhedron;
                 else
                     % compute anew
-                    prob = obj(i).Data.OptProb;
-                    primal = obj(i).Functions('primal-kkt'); % F*x+g
-                    dual = obj(i).Functions('dual-ineqlin'); % M*x+m
-                    % CR = { x | A*primal<=b+pB*x, dual>=0 }
-                    % CR = { x | A*(F*x+g)<=b+pB*x, M*x+m>=0 }
-                    % CR = { x | (A*F-pB)*x<=b-A*g, -M*x<=m }
-                    % CR = { x | H*x<=h }
-                    H = [ prob.A*primal.F-prob.pB; -dual.F ];
-                    h = [ prob.b-prob.A*primal.g; dual.g ];
-                    if prob.me>0
-                        He = prob.Ae*primal.F-prob.pE;
-                        he = prob.be-prob.Ae*primal.g;
-                        P(i) = Polyhedron('H', [H h], 'He', [He he]);
+                    [A, b, Ae, be] = obj(i).getHalfspaces();
+                    if isempty(be)
+                        P(i) = Polyhedron(A, b);
                     else
-                        P(i) = Polyhedron(H, h);
+                        P(i) = Polyhedron('H', [A b], 'He', [Ae be]);
                     end
                     P(i).copyFunctionsFrom(obj(i));
                     obj(i).Internal.Polyhedron = P(i);
@@ -52,15 +42,27 @@ classdef IPDPolyhedron < Polyhedron
         end
         
         function tf = isEmptySet(obj)
+            % Checks if a given set is empty
+
             tf = true(size(obj));
             for i = 1:numel(obj)
                 if ~isempty(obj(i).Internal.Empty)
                     tf(i) = obj(i).Internal.Empty;
                 else
-                    tf(i) = isEmptySet(toPolyhedron(obj(i)));
+                    % slow but simple:
+                    % tf(i) = isEmptySet(toPolyhedron(obj(i)));
+                    
+                    % faster: solve a feasibility LP
+                    [S.A, S.b, S.Ae, S.be] = obj(i).getHalfspaces();
+                    S.f = zeros(size(S.A, 2), 1);
+                    S.lb = []; S.ub = []; S.quicklp = true;
+                    sol = mpt_solve(S);
+                    tf(i) = ~isequal(sol.how, 'ok');
+                    obj(i).Internal.Empty = tf(i);
                 end
             end
         end
+        
         function tf = isFullDim(obj)
             tf = isFullDim(toPolyhedron(obj));
         end
@@ -111,6 +113,27 @@ classdef IPDPolyhedron < Polyhedron
         
         function h = plot_internal(obj, varargin)
             h = plot_internal(toPolyhedron(obj), varargin{:});
+        end
+        
+        function [A, b, Ae, be] = getHalfspaces(obj)
+            % Returns the half-space representation of the implicit
+            % polyhedron
+            
+            prob = obj.Data.OptProb;
+            primal = obj.Functions('primal-kkt'); % F*x+g
+            dual = obj.Functions('dual-ineqlin'); % M*x+m
+            % CR = { x | A*primal<=b+pB*x, dual>=0 }
+            %    = { x | A*(F*x+g)<=b+pB*x, M*x+m>=0 }
+            %    = { x | (A*F-pB)*x<=b-A*g, -M*x<=m }
+            %    = { x | H*x<=h }
+            A = [ prob.A*primal.F-prob.pB; -dual.F ];
+            b = [ prob.b-prob.A*primal.g; dual.g ];
+            if prob.me>0
+                Ae = prob.Ae*primal.F-prob.pE;
+                be = prob.be-prob.Ae*primal.g;
+            else
+                Ae = []; be = [];
+            end
         end
 	end
     
